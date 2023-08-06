e0712ebd4ead1bb6018c1299e464aeda5c3d716c
jersonb <jersonlbrito@hotmail.com>
2023-08-06 11:48:30 -0300

    feat: add controller phrases

---
+++ src/ExampleToday.Api/Controllers/PhrasesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExampleToday.Api.Controllers
+{
+    [ApiController]
+    [Route("phrases")]
+    public class PhrasesController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Ok!";
+        }
+    }
+}
