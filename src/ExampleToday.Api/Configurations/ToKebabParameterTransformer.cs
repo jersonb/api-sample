0a22a65b80d448a8f9628723edb120be5fd3986c
Jerson Brito <jerson.silva@ambevtech.com.br>
2023-08-19 15:46:31 -0300

    feat: configure paths to kebab case close #29

---
+++ src/ExampleToday.Api/Configurations/ToKebabParameterTransformer.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using System.Text.RegularExpressions;
+
+namespace ExampleToday.Api.Configurations
+{
+    public class ToKebabParameterTransformer : IOutboundParameterTransformer
+    {
+        public static RouteTokenTransformerConvention KebabTransform => new(new ToKebabParameterTransformer());
+
+        public string TransformOutbound(object? value)
+        {
+            var path = value?.ToString();
+            if (string.IsNullOrEmpty(path))
+            {
+                return "";
+            }
+
+            return Regex.Replace(path, "([a-z])([A-Z])", "$1-$2").ToLower();
+        }
+    }
+}
